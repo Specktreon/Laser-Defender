24ed22a5f14b730ce3f35ad9a8ca942858412e60
Specktreon <lezzar37@gmail.com>
2025-01-12 23:41:25 +0100

    Creating 2 Paths for enemies

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
-public class NewBehaviourScript : ScriptableObject
+public class WaveConfigSO : ScriptableObject
 {
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
