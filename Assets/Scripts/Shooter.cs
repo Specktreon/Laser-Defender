7ba97618a26f1dcfa5289182e3fd9020916673ec
Specktreon <lezzar37@gmail.com>
2025-01-14 02:46:33 +0100

    player shooting projectiles

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,6 +8,7 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5f;
+    [SerializeField] float firingRate = 0.2f;
 
     public bool isFiring;
 
@@ -25,19 +26,37 @@ public class Shooter : MonoBehaviour
 
     void Fire()
     {
-        if (isFiring)
+        if (isFiring && firingCoroutine == null)
         {
-            StartCoroutine(FireContinuously());
+            firingCoroutine =  StartCoroutine(FireContinuously());
 
         }
-        else
+        else if(!isFiring && firingCoroutine != null)
         {
-            StopCoroutine()
+            StopCoroutine(firingCoroutine);
+            firingCoroutine = null;
         }
     }
 
     IEnumerator FireContinuously()
     {
+        while(true)
+        {
+            GameObject instance = Instantiate(projectilePrefab,
+                                                transform.position,
+                                                Quaternion.identity);
+
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+            if(rb != null)
+            {
+                rb.velocity = transform.up * projectileSpeed;
+            }
+
+
+
+            Destroy(instance, projectileLifetime);
+            yield return new WaitForSeconds(firingRate);
+        }
 
     }
 }
